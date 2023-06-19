6cdd044193abe48c6ef40a2f086af9767e62d7fd
Stu-Gotz <gamer691@gmail.com>
2023-06-19 14:21:47 +0100

    added account and register pages. split store into smaller stores. added report just to keep everything in one place

---
+++ src/views/Register.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+function register() {
+  console.log('registered');
+}
+</script>
+
+<template>
+  <form @submit.prevent="register">
+    <div class="input-group">
+      <label class="input-group-label" for="username">Username: </label>
+      <input class="input-group-control" type="text" name="username" id="">
+    </div>
+
+    <div class="input-group">
+      <label class="input-group-label" for="email">Email: </label>
+      <input class="input-group-control" type="email" name="email" id="">
+    </div>
+    <div class="input-group">
+      <label class="input-group-label" for="emailconf">Confirm email: </label>
+      <input class="input-group-control" type="email" name="emailconf" id="">
+    </div>
+    <div class="form-text">Must be a valid email.</div>
+
+    <div class="input-group">
+      <label class="input-group-label" for="password">Password: </label>
+      <input class="input-group-control" type="text" name="password">
+    </div>
+    <div class="input-group">
+      <label class="input-group-label" for="passconf">Confirm password: </label>
+      <input class="input-group-control" type="text" name="passconf">
+    </div>
+    <div class="form-text">Password should use one digit and one special character.</div>
+
+  </form>
+</template>
