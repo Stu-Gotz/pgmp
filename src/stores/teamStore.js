64b0895c33b38608b2f67cd5c41dc4f6af83ba5f
Alan Nardo <alan.nardo.91@gmail.com>
2024-03-19 01:07:22 +0000

    problems with pushing

--- src/stores/teamStore.js
+++ src/stores/teamStore.js
@@ -3,7 +3,6 @@ import { Koffing } from 'koffing'
 import Pokedex from 'pokedex-promise-v2'
 
 
-
 export const useTeamStore = defineStore('teamStore', {
   state: () => ({
     team: null,
@@ -15,12 +14,45 @@ export const useTeamStore = defineStore('teamStore', {
     }
   },
   actions: {
+ 
     async parseInput(teamInput) {
-      
+
+      const that = this;
+      function lowerCaseKeys(obj) {
+
+        Object.keys(obj).forEach(k => {
+          if (typeof obj[k] == 'string') {
+            obj[k] = obj[k].toLowerCase();
+          }
+          else if (typeof obj[k] == 'object') {
+            obj[k] = lowerCaseKeys(obj[k])
+          }
+        });
+        return obj;
+      }
+
+      async function mapMoves(movesArray, that){
+        console.log(movesArray)
+        const moveObjects = await that.P.getMoveByName(movesArray.map(i => i.replace(' ','-')));
+  
+        console.log(moveObjects)
+      }
+
       const parsedTeam = Koffing.parse(teamInput);
       const members = parsedTeam.teams[0].pokemon
-      const tasks = members.map(pokemon => pokemon.name.toLowerCase()) 
-      const pokemon = await this.P.getPokemonByName(tasks)
+      const formattedMembers = members.map(function(el) {
+        const formatted = lowerCaseKeys(el)
+        return formatted
+      });
+      console.log(formattedMembers)
+      const tasks = formattedMembers.map(pokemon => pokemon.name.toLowerCase()) 
+      let pokemon = await this.P.getPokemonByName(tasks)
+
+      formattedMembers.forEach((el) => {
+        mapMoves(el['moves'], that)
+      })
+
+
       console.log(pokemon)
     },
 
